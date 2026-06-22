21b87e40f1fbb11db21915c04a72fc7a0fa53fad
Parikar Agarwal <parikaragarwal@gmail.com>
2026-06-22 16:59:14 +0530

    some changes made

--- packages/trpc/server/routes/health/route.ts
+++ packages/trpc/server/routes/health/route.ts
@@ -1,5 +1,28 @@
 import { z, zodUndefinedModel } from "../../schema";
 import { publicProcedure, router } from "../../trpc";
+import { generatePath } from "../../utils/path-generator";
+
+const getPath = generatePath("/health");
+const TAGS = ["Health"];
 
 export const healthRouter = router({
+    checkHealth: publicProcedure
+    .meta({
+        openapi: {
+            method: "GET",
+            path: getPath('/health'),
+            tags: TAGS, 
+        },
+    })
+    .input(zodUndefinedModel)
+    .output(z.object({
+        status: z.string(),
+        message:z.string()
+    }))
+    .query(()=>{
+        return {
+           status: "ok",
+           message:"Server is up and running"
+        }
+    })
 });
